55b26907db80cc7e07e76d3206e209154bdf0a88
BlueBug <iopzxz@gmail.com>
2017-05-13 20:44:57 -0400

    Added Sphercial Camera : Pointlight should extend from this

--- DX11Starter/Files/Graphic/Scene/cpp/_GraphicScene.cpp
+++ DX11Starter/Files/Graphic/Scene/cpp/_GraphicScene.cpp
@@ -3,6 +3,7 @@
 #include <Graphic\Scene\PointLight.h>
 #include <Graphic\Scene\ILight.h>
 #include <Graphic\Scene\Camera.h>
+#include <Graphic\Scene\SphericalCamera.h>
 using namespace NGraphic;
 using namespace NGraphic::NScene;
 
@@ -139,6 +140,90 @@ Object Camera::setRotation(Quaternion quaternion)
 	m_isDirty_matView = true;
 	return *this;
 
+}
+
+void SphericalCamera::updatePointLightViewMatrixs()
+{
+	Camera::getViewMatrix();
+	m_matLookXPlus = DirectX::XMMatrixLookToLH(m_pos, Vector3(1, 0, 0), Vector3(0, 1, 0));
+	m_matLookXMinus = DirectX::XMMatrixLookToLH(m_pos, Vector3(-1, 0, 0), Vector3(0, 1, 0));
+	m_matLookYPlus = DirectX::XMMatrixLookToLH(m_pos, Vector3(0, 1, 0), Vector3(0, 0, -1));
+	m_matLookYMinus = DirectX::XMMatrixLookToLH(m_pos, Vector3(0, -1, 0), Vector3(0, 0, 1));
+	m_matLookZPlus = DirectX::XMMatrixLookToLH(m_pos, Vector3(0, 0, 1), Vector3(0, 1, 0));
+	m_matLookZMinus = DirectX::XMMatrixLookToLH(m_pos, Vector3(0, 0, -1), Vector3(0, 1, 0));
+}
+
+NGraphic::NScene::SphericalCamera::SphericalCamera()
+{
+	Camera();
+	setFOV(3.14 / 2 + 0.15f);
+}
+
+
+Matrix SphericalCamera::getViewMatrix()
+{
+	if (m_isDirty_matView) {
+		updatePointLightViewMatrixs();
+		//update my other matrix as well
+	}
+	return Camera::getViewMatrix();
+}
+
+Matrix SphericalCamera::getMatrixXPlus()
+{
+	if (m_isDirty_matView) {
+		updatePointLightViewMatrixs();
+		m_isDirty_matView = false;
+		//update my other matrix as well
+	}
+	return m_matLookXPlus;
+}
+Matrix SphericalCamera::getMatrixXMinus()
+{
+	if (m_isDirty_matView) {
+		updatePointLightViewMatrixs();
+		m_isDirty_matView = false;
+		//update my other matrix as well
+	}
+	return m_matLookXMinus;
+}
+
+Matrix SphericalCamera::getMatrixYPlus()
+{
+	if (m_isDirty_matView) {
+		updatePointLightViewMatrixs();
+		m_isDirty_matView = false;
+		//update my other matrix as well
+	}
+	return m_matLookYPlus;
+}
+Matrix SphericalCamera::getMatrixYMinus()
+{
+	if (m_isDirty_matView) {
+		updatePointLightViewMatrixs();
+		m_isDirty_matView = false;
+		//update my other matrix as well
+	}
+	return m_matLookYMinus;
+}
+
+Matrix SphericalCamera::getMatrixZPlus()
+{
+	if (m_isDirty_matView) {
+		updatePointLightViewMatrixs();
+		m_isDirty_matView = false;
+		//update my other matrix as well
+	}
+	return m_matLookZPlus;
+}
+Matrix SphericalCamera::getMatrixZMinus()
+{
+	if (m_isDirty_matView) {
+		updatePointLightViewMatrixs();
+		m_isDirty_matView = false;
+		//update my other matrix as well
+	}
+	return m_matLookZMinus;
 }
 
 //Light
